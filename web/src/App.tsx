4af5b742c458c387e4ce5d002e19a811ab38ce87
Rosario Lucas <lucasrosario1@hotmail.com>
2023-01-18 23:27:43 -0300

    Criado back e front setup

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import './styles/global.css';
+
+import { Habit } from './components/Habit'
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={10}/>
+      <Habit completed={20}/>
+      <Habit completed={30}/>
+      
+    </div>
+    
+  )
+}
+
+export default App
+
+// Componente: Reaproveitar / Isolar
+// Propriedade: Uma informacaoi enviada pra modificar um componente visual ou comportalmente
